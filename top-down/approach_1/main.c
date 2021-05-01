4835602e32a59fe0724c0832755dd7d6e5e6e57f
Paulus Gandung Prakosa <rvn.plvhx@gmail.com>
2021-05-01 11:57:20 +0000

    Reformatting top-down/approach_1/main.c

--- top-down/approach_1/main.c
+++ top-down/approach_1/main.c
@@ -5,81 +5,81 @@
 #define unused(x) ((void)(x))
 
 static void test_case_0(void) {
-	parser_state_t *pst = parse_buffer("foo_bar_baz");
+  parser_state_t *pst = parse_buffer("foo_bar_baz");
 
-	if (pst == NULL) {
-		return;
-	}
+  if (pst == NULL) {
+    return;
+  }
 
-	printf("----- %s -----\n", __func__);
-	parser_dump(pst);
-	printf("\n");
+  printf("----- %s -----\n", __func__);
+  parser_dump(pst);
+  printf("\n");
 
-	parser_dtor(pst);
+  parser_dtor(pst);
 }
 
 static void test_case_1(void) {
-	parser_state_t *pst = parse_buffer("(foo_bar_baz)");
+  parser_state_t *pst = parse_buffer("(foo_bar_baz)");
 
-	if (pst == NULL) {
-		return;
-	}
+  if (pst == NULL) {
+    return;
+  }
 
-	printf("----- %s -----\n", __func__);
-	parser_dump(pst);
-	printf("\n");
+  printf("----- %s -----\n", __func__);
+  parser_dump(pst);
+  printf("\n");
 
-	parser_dtor(pst);
+  parser_dtor(pst);
 }
 
 static void test_case_2(void) {
-	parser_state_t *pst = parse_buffer("((foo_bar_baz))");
+  parser_state_t *pst = parse_buffer("((foo_bar_baz))");
 
-	if (pst == NULL) {
-		return;
-	}
+  if (pst == NULL) {
+    return;
+  }
 
-	printf("----- %s -----\n", __func__);
-	parser_dump(pst);
-	printf("\n");
+  printf("----- %s -----\n", __func__);
+  parser_dump(pst);
+  printf("\n");
 
-	parser_dtor(pst);
+  parser_dtor(pst);
 }
 
 static void test_case_3(void) {
-	parser_state_t *pst = parse_buffer("(((foo_bar_baz)))");
+  parser_state_t *pst = parse_buffer("(((foo_bar_baz)))");
 
-	if (pst == NULL) {
-		return;
-	}
+  if (pst == NULL) {
+    return;
+  }
 
-	printf("----- %s -----\n", __func__);
-	parser_dump(pst);
-	printf("\n");
+  printf("----- %s -----\n", __func__);
+  parser_dump(pst);
+  printf("\n");
 
-	parser_dtor(pst);
+  parser_dtor(pst);
 }
 
 static void test_case_4(void) {
-	parser_state_t *pst = parse_buffer("((((foo_bar_baz))))");
+  parser_state_t *pst = parse_buffer("((((foo_bar_baz))))");
 
-	if (pst == NULL) {
-		return;
-	}
+  if (pst == NULL) {
+    return;
+  }
 
-	printf("----- %s -----\n", __func__);
-	parser_dump(pst);
-	printf("\n");
+  printf("----- %s -----\n", __func__);
+  parser_dump(pst);
+  printf("\n");
 
-	parser_dtor(pst);
+  parser_dtor(pst);
 }
 
 static void test_case_5__closing_parenthesis_overload(void) {
-	parser_state_t *pst = parse_buffer("((((foo_bar_baz)))))");
+  parser_state_t *pst = parse_buffer("((((foo_bar_baz)))))");
 }
 
 static void test_case_5__opening_parenthesis_overload(void) {
-	parser_state_t *pst = parse_buffer("(((((foo_bar_baz))))");
+  parser_state_t *pst = parse_buffer("(((((foo_bar_baz))))");
 }
 
 int main(int argc, char **argv) {
